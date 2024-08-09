7634852d75a5f412a0c6e8bf6d38bc13f1b1d76e
Nam9227 <vn5753378@gmail.com>
2024-08-09 12:11:08 +0700

    Create register.js

---
+++ register.js
@@ -0,0 +1,22 @@
+document.getElementById('register-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  
+  const username = document.getElementById('new-username').value;
+  const password = document.getElementById('new-password').value;
+
+  if(username && password) {
+    const user = {
+      username: username,
+      password: password
+    };
+
+    // Lưu tài khoản vào localStorage
+    localStorage.setItem(username, JSON.stringify(user));
+    alert('Đăng ký thành công! Bạn có thể đăng nhập bây giờ.');
+
+    // Điều hướng về trang đăng nhập
+    window.location.href = 'login.html';
+  } else {
+    alert('Vui lòng nhập đầy đủ thông tin.');
+  }
+});
