53fbde7d64e97d939c1d02cc6c79eb553246e88d
Nam9227 <vn5753378@gmail.com>
2024-08-10 10:54:17 +0700

    Update home.js

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Kiểm tra trạng thái đăng nhập
-    const isLoggedIn = localStorage.getItem('loggedIn'); // Hoặc sessionStorage
+    const isLoggedIn = localStorage.getItem('loggedIn');
 
     if (!isLoggedIn) {
         // Nếu chưa đăng nhập, chuyển hướng đến trang đăng nhập
