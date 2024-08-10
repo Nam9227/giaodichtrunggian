8df3c24a49556d5e9c87d93f067e7d7f014375fe
Nam9227 <vn5753378@gmail.com>
2024-08-10 16:28:25 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,5 @@
-export const firebaseConfig = {
+// firebase-config.js
+const firebaseConfig = {
   apiKey: "AIzaSyDo5POI_MkDJyNE5y_7BIdfs-B2mj1iUBY",
   authDomain: "csdl-web-giaodich.firebaseapp.com",
   databaseURL: "https://csdl-web-giaodich-default-rtdb.firebaseio.com",
@@ -8,3 +9,5 @@ export const firebaseConfig = {
   appId: "1:834585121842:web:bca26240e1e5187792d82b",
   measurementId: "G-5VDD2LKEKV"
 };
+
+export { firebaseConfig };
