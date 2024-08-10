80463197830270cf85e87ad5e6e69366277c4b1d
Nam9227 <vn5753378@gmail.com>
2024-08-10 16:57:34 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,25 @@
-import { database, auth } from './firebase-config.js';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDo5POI_MkDJyNE5y_7BIdfs-B2mj1iUBY",
+  authDomain: "csdl-web-giaodich.firebaseapp.com",
+  databaseURL: "https://csdl-web-giaodich-default-rtdb.firebaseio.com",
+  projectId: "csdl-web-giaodich",
+  storageBucket: "csdl-web-giaodich.appspot.com",
+  messagingSenderId: "834585121842",
+  appId: "1:834585121842:web:bca26240e1e5187792d82b",
+  measurementId: "G-5VDD2LKEKV"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+const auth = firebase.auth();
+
+export { database, auth };
+
 
 window.onload = function() {
   // Tạo tài khoản admin mặc định nếu chưa tồn tại
