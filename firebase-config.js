9d0eaeb7d65a8fcfc489586642f4ff824a96762c
Nam9227 <vn5753378@gmail.com>
2024-08-10 13:42:53 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDo5POI_MkDJyNE5y_7BIdfs-B2mj1iUBY",
+  authDomain: "csdl-web-giaodich.firebaseapp.com",
+  databaseURL: "https://csdl-web-giaodich-default-rtdb.firebaseio.com",
+  projectId: "csdl-web-giaodich",
+  storageBucket: "csdl-web-giaodich.appspot.com",
+  messagingSenderId: "834585121842",
+  appId: "1:834585121842:web:bca26240e1e5187792d82b",
+  measurementId: "G-5VDD2LKEKV"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
