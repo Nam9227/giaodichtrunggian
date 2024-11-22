39240b314230f4ec1c1b9b1b4c230b79839da6d7
Nam9227 <vn5753378@gmail.com>
2024-11-22 22:13:16 +0700

    Delete home.js

--- home.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Kiểm tra trạng thái đăng nhập
-    const isLoggedIn = localStorage.getItem('loggedIn');
-
-    if (!isLoggedIn) {
-        // Nếu chưa đăng nhập, chuyển hướng đến trang đăng nhập
-        window.location.href = 'login.html';
-    }
-});
