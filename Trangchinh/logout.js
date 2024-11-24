655dfe81fc6ecfa64961c0723ee5e96388841776
Nam9227 <vn5753378@gmail.com>
2024-11-24 20:48:53 +0700

    Create logout.js

---
+++ Trangchinh/logout.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const logoutBtn = document.getElementById('logout-btn');
+
+    if (logoutBtn) {
+        logoutBtn.addEventListener('click', () => {
+            // Xóa thông tin đăng nhập
+            localStorage.removeItem('loggedIn');
+            localStorage.removeItem('isAdmin');
+
+            // Chuyển hướng về trang đăng nhập
+            window.location.href = '../login.html';
+        });
+    }
+});
